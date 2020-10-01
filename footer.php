4975d443c1577b598fd0ed6af5ff32b3e97e401d
Brent Forwood <forwoodb77@gmail.com>
2020-10-01 16:52:21 -0400

    Added food app to portfolio, food app image

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <a href="https://www.github.com/forwoodb"><img src="img/github.png" alt="Github Logo" class="social-icon"></a>
 <a href="https://www.linkedin.com/in/brentforwood"><img src="img/linkedin-wrap.png" alt="LinkedIn Logo" class="social-icon"></a>
 <a href="https://twitter.com/brentforwood1"><img src="img/twitter-wrap.png" alt="Twitter Logo" class="social-icon"></a>
-<a href="http://facebook.com/brentforwood"><img src="img/facebook-wrap.png" alt="Facebook Logo" class="social-icon"></a>
+<a href="https://facebook.com/brentforwood"><img src="img/facebook-wrap.png" alt="Facebook Logo" class="social-icon"></a>
 <p>&copy; 2020 Brent Forwood.</p>
--- index.php
+++ index.php
@@ -10,31 +10,37 @@
             </a>
           </li>
           <li>
+            <a href="https://foodapp-ce8b6.firebaseapp.com/" target="_blank">
+              <img src="img/food-app-react.png" alt="">
+              <p>Food App - React</p>
+            </a>
+          </li>
+          <li>
             <a href="https://listapp-24506.firebaseapp.com/" target="_blank">
               <img src="img/list-app-react.png" alt="">
               <p>List App - React</p>
             </a>
           </li>
           <li>
-            <a href="http://brentforwood.com/DrivingApp/index.php" target="_blank">
+            <a href="https://brentforwood.com/DrivingApp/index.php" target="_blank">
               <img src="img/driving-app.png" alt="">
               <p>Driving App</p>
             </a>
           </li>
           <li>
-            <a href="http://brentforwood.com/FoodApp/index.php" target="_blank">
+            <a href="https://brentforwood.com/FoodApp/index.php" target="_blank">
               <img src="img/food-app.png" alt="">
               <p>Food App</p>
             </a>
           </li>
           <li>
-            <a href="http://brentforwood.com/ToDoApp/index.php" target="_blank">
+            <a href="https://brentforwood.com/ToDoApp/index.php" target="_blank">
               <img src="img/todo-app.png" alt="">
               <p>To Do App</p>
             </a>
           </li>
           <li>
-            <a href="http://forwoodb.github.io/one-page-theme/" target="_blank">
+            <a href="https://forwoodb.github.io/one-page-theme/" target="_blank">
               <img src="img/one-page-theme.png" alt="">
               <p>Single Page Small Business Theme</p>
             </a>
@@ -52,7 +58,7 @@
             </a>
           </li> -->
           <li>
-            <a href="http://forwoodb.github.io/htmaw/" target="_blank">
+            <a href="https://forwoodb.github.io/htmaw/" target="_blank">
               <img src="img/htmaw.png" alt="">
               <p>How to Make a Website</p>
             </a>

92ba42a82ac0dd610c3d6194f13c96e557900bbe
Brent Forwood <forwoodb77@gmail.com>
2020-10-01 17:16:07 -0400

    Fixed git issue

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
             </a>
           </li>
           <li>
-<<<<<<< HEAD
             <a href="https://foodapp-ce8b6.firebaseapp.com/" target="_blank">
               <img src="img/food-app-react.png" alt="">
               <p>Food App - React</p>
@@ -30,7 +29,6 @@
           </li>
           <li>
             <a href="https://brentforwood.com/FoodApp/index.php" target="_blank">
-=======
             <a href="https://listapp-24506.firebaseapp.com/" target="_blank">
               <img src="img/list-app-react.png" alt="">
               <p>List App - React</p>
@@ -44,27 +42,20 @@
           </li>
           <li>
             <a href="http://brentforwood.com/FoodApp/index.php" target="_blank">
->>>>>>> Various updates
               <img src="img/food-app.png" alt="">
               <p>Food App</p>
             </a>
           </li>
           <li>
-<<<<<<< HEAD
             <a href="https://brentforwood.com/ToDoApp/index.php" target="_blank">
-=======
             <a href="http://brentforwood.com/ToDoApp/index.php" target="_blank">
->>>>>>> Various updates
               <img src="img/todo-app.png" alt="">
               <p>To Do App</p>
             </a>
           </li>
           <li>
-<<<<<<< HEAD
             <a href="https://forwoodb.github.io/one-page-theme/" target="_blank">
-=======
             <a href="http://forwoodb.github.io/one-page-theme/" target="_blank">
->>>>>>> Various updates
               <img src="img/one-page-theme.png" alt="">
               <p>Single Page Small Business Theme</p>
             </a>
@@ -82,11 +73,8 @@
             </a>
           </li> -->
           <li>
-<<<<<<< HEAD
             <a href="https://forwoodb.github.io/htmaw/" target="_blank">
-=======
             <a href="http://forwoodb.github.io/htmaw/" target="_blank">
->>>>>>> Various updates
               <img src="img/htmaw.png" alt="">
               <p>How to Make a Website</p>
             </a>
